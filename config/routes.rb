6df82fd518d7cddc4b2af19ead4e6a79b64715a4
joseph Borremans <joseph.borremans@gmail.com>
2012-11-12 16:11:44 +0100

    delete comand

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Todo::Application.routes.draw do
   
+  root :to => 'todos#index'
   get "todos/index"
 
   get "pages/products"
@@ -59,7 +60,7 @@ Todo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  
 
   # See how all your routes lay out with "rake routes"
 
